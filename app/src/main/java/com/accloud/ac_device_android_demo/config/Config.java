e369de1f90a0679c0d210fcbede415e4fb909074
wangkun <wangkun@ablecloud.cn>
2017-04-10 19:52:20 +0800

    Init commit in GitHub
    Signed-off-by: wangkun <wangkun@ablecloud.cn>

---
+++ app/src/main/java/com/accloud/ac_device_android_demo/config/Config.java
@@ -0,0 +1,17 @@
+package com.accloud.ac_device_android_demo.config;
+
+/**
+ * Created by Administrator on 2015/8/26.
+ */
+public class Config {
+    //设备上报的code
+    public static final int CODE_REPORT = 203;
+
+    //实际开发请只选择以下其中一种消息格式
+    //二进制
+    public static final int CODE_SWITCH_REQ = 68;
+    public static final int CODE_SWITCH_RESP = 102;
+    //json
+    public static final int CODE_JSON = 70;
+}
+
